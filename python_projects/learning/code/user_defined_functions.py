843927fca88bf437370414965d2d8950739f4a3e
hankpeeples <hgpeeples48@gmail.com>
2020-06-14 21:48:37 -0500

    added info

--- python_projects/learning/code/user_defined_functions.py
+++ python_projects/learning/code/user_defined_functions.py
@@ -1,8 +1,24 @@
 
 
+#---defining function
 #def = define function
 def my_function():
     print("\nThis is my_function!")
     print("A second string\n")
 
 my_function()
+
+#---adding arguments to function
+def my_function2(str1, str2):
+    print(f'\t{str1}')
+    print(f'\t{str2}\n')
+
+my_function2("Argument 1", "Argument 2")
+
+
+#default Arguments
+def print_something(name = 'name', age = 'unknown'):
+    print("My name is", name, "and my age is", age)
+
+print_something() #default
+print_something('Hank', 20) #given values
